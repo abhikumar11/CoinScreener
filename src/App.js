8c1ad0b69d08d3763bbe4b69afd1b2513b300fc8
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-19 11:16:02 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import { Switch } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Coins from "./components/Coins";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
+const useStyles = makeStyles(()=>({
+  App: {
+    backgroundColor: '#14161a',
+    color: 'white',
+    minHeight:'100vh',
+  },
+}));
 function App() {
+
+  const classes = useStyles();
      return (
           <BrowserRouter>
+          <div className={classes.App}>
                <Header />
                <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/coins" element={<Coins />} />
+                    <Route path="/coins/:id" element={<Coins />} />
                </Routes>
+               </div>
           </BrowserRouter>
+
      );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+import { AppBar, Container } from "@material-ui/core";
 import React from "react";
 
 function Header() {
-     return <div><p>Header</p></div>;
+     return (
+     <AppBar color="transparent" position="static">
+          <Container>
+               
+          </Container>
+     </AppBar>
+     );
 }
 
 export default Header;
