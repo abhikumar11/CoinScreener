0097b2bd60716b5b0b75e7c437a618baa0ecd861
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-19 14:38:50 +0530

    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react'
+ 
+const useStyles = makeStyles(()=>({
+    
+}));
+function Banner() {
+    const classes=useStyles();
+  return (
+    <div className={classes}>
+
+    </div>
+  )
+}
+
+export default Banner
