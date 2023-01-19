70d4d6c9ab4bd12007f5cefb09fc350e5fabe4a8
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-19 15:39:35 +0530

    initial commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,16 +1,55 @@
-import { makeStyles } from '@material-ui/core';
-import React from 'react'
- 
-const useStyles = makeStyles(()=>({
-    
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles(() => ({
+     banner: {
+          background: "URL(./backimage.jpg)",
+     },
+     bannercontent: {
+          height: 400,
+          display: "flex",
+          flexDirection: "column",
+          paddingTop: 25,
+          justifyContent: "space-around",
+     },
+     tagline:{
+        display: "flex",
+        height: "40%",
+        flexDirection: "column",
+        justifyContent: "center",
+        textAlign: "center",
+     }
 }));
 function Banner() {
-    const classes=useStyles();
-  return (
-    <div className={classes}>
-
-    </div>
-  )
+     const classes = useStyles();
+     return (
+          <div className={classes.banner}>
+               <Container className={classes.bannercontent}>
+                    <div className={classes.tagline}>
+                         <Typography
+                              variant="h2"
+                              style={{
+                                   fontWeight: "bold",
+                                   marginBottom: 15,
+                                   fontFamily: "Montserrat",
+                              }}
+                         >
+                              Crypto Screener
+                         </Typography>
+                         <Typography
+                              variant="subtitle2"
+                              style={{
+                                   color: "gold",
+                                   textTransform:'capitalize',
+                                   fontFamily: "Montserrat",
+                              }}
+                         >
+                              Get all the information about cryptocurrencies 
+                         </Typography>
+                    </div>
+               </Container>
+          </div>
+     );
 }
 
-export default Banner
+export default Banner;
