3693a39504770962d459d5130ee6118f6e8f9fe6
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-19 09:22:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-import Coins from './components/Coins';
-import Header from './components/Header';
-import Home from './components/Home';
+import { Switch } from "@material-ui/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Coins from "./components/Coins";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Header/>
-    <Route path='/' component={Home}/>
-    <Route path='/coins/' component={Coins}/> 
-    </BrowserRouter>
-  );
+     return (
+          <BrowserRouter>
+               <Header />
+               <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/coins" element={<Coins />} />
+               </Routes>
+          </BrowserRouter>
+     );
 }
 
 export default App;
