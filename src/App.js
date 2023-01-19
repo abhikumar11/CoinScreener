523f3681b88465e4548d1b8822456e01d582f802
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-19 08:59:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Coins from './components/Coins';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Route path='/' component={Home}/>
+    <Route path='/coins/' component={Coins}/> 
+    </BrowserRouter>
   );
 }
 
