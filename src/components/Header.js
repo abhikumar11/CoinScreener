d8ac6d1afb81d1e79d5707eff54d4772b0cd7b37
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-19 12:07:52 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,62 @@
-import { AppBar, Container } from "@material-ui/core";
+import {
+     AppBar,
+     Container,
+     createTheme,
+     makeStyles,
+     MenuItem,
+     Select,
+     ThemeProvider,
+     Toolbar,
+     Typography,
+} from "@material-ui/core";
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+const useStyles = makeStyles(() => ({
+     title: {
+          flex: 1,
+          color: "gold",
+          fontFamily: "Montserrat",
+          fontWeight: "bold",
+          cursor: "pointer",
+     },
+}));
 function Header() {
+     const classes = useStyles();
+     const history = useNavigate();
+     const darkTheme = createTheme({
+          palette: {
+               primary: {
+                    main: "#fff",
+               },
+               type: "dark",
+          },
+     });
      return (
-     <AppBar color="transparent" position="static">
-          <Container>
-               
-          </Container>
-     </AppBar>
+          <ThemeProvider theme={darkTheme}>
+               <AppBar color="transparent" position="static">
+                    <Container>
+                         <Toolbar>
+                              <Typography
+                                   onClick={() => history("/")}
+                                   className={classes.title} variant='h6'
+                              >
+                                   Coin Screener
+                              </Typography>
+                              <Select
+                                   variant="outlined"
+                                   style={{
+                                        width: 100,
+                                        height: 40,
+                                        marginRight: 15,
+                                   }}
+                              >
+                                   <MenuItem value={"USD"}>USD</MenuItem>
+                                   <MenuItem value={"INR"}>INR</MenuItem>
+                              </Select>
+                         </Toolbar>
+                    </Container>
+               </AppBar>
+          </ThemeProvider>
      );
 }
 
