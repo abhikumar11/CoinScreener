ce59c3df01d6b35f9c8c3eec6fc028f3bfdf74b0
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-20 20:32:01 +0530

    initial commit

---
+++ src/components/CoinTable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CoinTable() {
+  return (
+    <div></div>
+  )
+}
+
+export default CoinTable
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Banner from './Banner';
-
+import CoinTable from './CoinTable';
 function Home() {
   return (
     <>
     <Banner/>
+    <CoinTable/>
     </>
   )
 }
