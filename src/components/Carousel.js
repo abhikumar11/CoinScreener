d1035a4be38c13b0817db1c580da15ea0b541357
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-20 17:44:01 +0530

    initial commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core";
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TrendingCoins } from "./Api";
 import { CryptoState } from "./Contex";
 
@@ -12,11 +12,17 @@ const useStyles = makeStyles(() => ({
      },
 }));
 function Carousel() {
+     const [trending, setTrending] = useState([]);
      const classes = useStyles();
      const { currency } = CryptoState();
      const fetchCoins = async () => {
           const { data } = await axios.get(TrendingCoins(currency));
+          setTrending(data);
      };
+     console.log(trending);
+     useEffect(() => {
+          fetchCoins();
+     }, [currency]);
      return <div className={classes.carousel}>Carousel</div>;
 }
 
