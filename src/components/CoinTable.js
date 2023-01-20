b3532bf645f0ea8d70299dfc124e26a2449642f2
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-20 20:47:01 +0530

    initial commit

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React,{ useState ,useEffect} from 'react'
+import { CoinList } from './Api';
+import { CryptoState } from './Contex';
 
 function CoinTable() {
+    const [coins,setCoins]=useState([]);
+    const[loading,setLoading] = useState(false);
+    const {currency}=CryptoState();
+    const  fetchCoins =async()=>{
+        setLoading(true);
+        const {data}=await axios.get(CoinList(currency));
+        setCoins(data);
+        setLoading(false);
+    }
+    useEffect(() => {
+      fetchCoins();
+    }, [currency]);
+    
+    
   return (
-    <div></div>
+    <div>
+        
+    </div>
   )
 }
-
 export default CoinTable
