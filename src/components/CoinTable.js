f14ef740fd06d8e98d1061a5f8fdea68b1612f30
Abhishek Kumar Verma <abhishekkumar12verma@gmail.com>
2023-01-21 09:49:56 +0530

    initial commit

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -1,3 +1,4 @@
+import { Container, createTheme, ThemeProvider, Typography } from '@material-ui/core';
 import axios from 'axios';
 import React,{ useState ,useEffect} from 'react'
 import { CoinList } from './Api';
@@ -12,16 +13,25 @@ function CoinTable() {
         const {data}=await axios.get(CoinList(currency));
         setCoins(data);
         setLoading(false);
-    }
+    };
     useEffect(() => {
       fetchCoins();
     }, [currency]);
     
-    
+  const darkTheme=createTheme({
+    palette:{
+      main:"#fff"
+    },
+    type:"dark"
+  })
   return (
-    <div>
-        
-    </div>
+    <ThemeProvider theme={darkTheme}>
+        <Container style={{textAlign:"center"}}>
+          <Typography variant='h4' style={{margin:18,fontFamily:"Montserrat"}}>
+          
+          </Typography>
+        </Container>
+    </ThemeProvider>
   )
 }
 export default CoinTable
